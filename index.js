20ef1b302d53dce8315d629585ec621915919367
faria-r <fariacse11@gmail.com>
2022-11-26 15:12:47 +0600

    APi Created for adding products

--- index.js
+++ index.js
@@ -23,55 +23,123 @@ app.get("/", async (req, res) => {
   res.send("Server is running");
 });
 //verify JWT
-function verifyJWT(req,res,next){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send("Unauthorized access");
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("Unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send("Forbidden Access");
     }
-    const token = authHeader.split(" ")[1];
-    jwt.verify(token,process.env.ACCESS_TOKEN,function(err, decoded){
-        if (err) {
-            return res.status(403).send("Forbidden Access");
-          }
-          req.decoded = decoded;
-          next();
-    })
+    req.decoded = decoded;
+    next();
+  });
 }
 async function run() {
   try {
-    const categoryCollection = client.db('beatsToRead').collection('books-categories');
-    const BooksCollection = client.db('beatsToRead').collection('allBooks');
-    const ordersCollection = client.db('beatsToRead').collection('orders');
-//api to get JWT token
+    const categoryCollection = client
+      .db("beatsToRead")
+      .collection("books-categories");
+    const BooksCollection = client.db("beatsToRead").collection("allBooks");
+    const ordersCollection = client.db("beatsToRead").collection("orders");
+    const usersCollection = client.db("beatsToRead").collection("users");
 
-app.post('/jwt',(req,res)=>{
-    const user = req.body;
-    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'30 days'})
-    res.send({token})
-})
-    //api to get all categories
-    app.get('/categories',async(req,res)=>{
-        const query = {};
-        const result = await categoryCollection.find(query).toArray();
-        res.send(result)
+    //verify admin
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        res.status(401).send("Unauthorized Access");
+      }
+      next();
+    };
+    //api to get JWT token
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "30D",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
     });
 
-    //api to get category based book
+    //api to get admin
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
 
-app.get('/categories/:name',async(req,res)=>{
-    const name = req.params.name;
-    const query = {
-        CategoryName:name
-        };
-    const result = await BooksCollection.find(query).toArray();
-    res.send(result);
-});
-//store users orders in database;
-app.post('/orders',async(req,res)=>{
-  const order = req.body;
-  const result = await ordersCollection.insertOne(order);
-  res.send(result)
-})
+    //api to get buyer
+    app.get('/users/buyer/:email',async(req,res) =>{
+      const email = req.params.email;
+      console.log(email)
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      console.log(user)
+      res.send({isBuyer: user?.role === 'Buyer'})
+    })
+    //api to get seller
+    app.get('/users/seller/:email',async(req,res) =>{
+      const email = req.params.email;
+      console.log(email)
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      console.log(user)
+      res.send({isSeller: user?.role === 'Seller'})
+    })
+    //api to get all categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const result = await categoryCollection.find(query).toArray();
+      res.send(result);
+    });
+    //api to store added products
+    app.post('/books',async(req,res)=>{
+      const books =req.body;
+      const result = await BooksCollection.insertOne(books);
+      res.send(result);
+    })
+
+    //api to get category based book
+    app.get("/categories/:name", async (req, res) => {
+      const name = req.params.name;
+      const query = {
+        CategoryName: name,
+      };
+      const result = await BooksCollection.find(query).toArray();
+      res.send(result);
+    });
+    //store users orders in database;
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+    //api to store users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const existingUserEmail = user.email;
+      const query = { email: existingUserEmail };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({message:"user alredy stored"});
+      } else {
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      }
+    });
+
+    //api to get all seller
+    app.get("/");
   } finally {
   }
 }
